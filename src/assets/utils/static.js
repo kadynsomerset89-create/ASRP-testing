af4d25d295963b22bc4e9582614bb1cf09279a52
kadynsomerset89-create <kadynsomerset89@gmail.com>
2026-01-20 17:06:30 -0800

    Update avatarURL to new image source

--- src/assets/utils/static.js
+++ src/assets/utils/static.js
@@ -7,5 +7,5 @@ module.exports.DummyUser = {
     name: "Unknown User",
     username: "Unknown User",
     hexAccentColor: "#FFFFFF",
-    avatarURL: () => 'https://cdn.discordapp.com/embed/avatars/0.png'
+    avatarURL: () => 'https://cdn.discordapp.com/attachments/1462621739979636915/1462634058407215196/Utah_Logo.png?ex=6970e1dd&is=696f905d&hm=736830a480010c4eb2541a19abff6aa68ef0b71e4ee9009f6b77b485742c791e&'
 }
